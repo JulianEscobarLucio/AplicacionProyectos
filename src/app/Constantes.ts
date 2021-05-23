70945beb5e0748b9173abb965ec8d1e6205a515c
julieslu <jescobar@ias.com.co>
2021-05-23 11:30:46 -0500

    .

--- src/app/Constantes.ts
+++ src/app/Constantes.ts
@@ -1,2 +1,2 @@
-export const servicePath = 'https://ms-gestion-proyecto-back.herokuapp.com/ms-gestion-proyectos';
-//export const servicePath = 'http://localhost:8080/ms-gestion-proyectos';
+//export const servicePath = 'https://ms-gestion-proyecto-back.herokuapp.com/ms-gestion-proyectos';
+export const servicePath = 'http://localhost:8080/ms-gestion-proyectos';
